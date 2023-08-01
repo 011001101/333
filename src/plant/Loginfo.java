a3d0df6814e86e0f9562b586a7413d065bd4f93e
GGG <GGG@DESKTOP-8N3GG4O>
2023-08-01 17:25:21 +0900

    a

--- src/plant/Loginfo.java
+++ src/plant/Loginfo.java
@@ -23,6 +23,7 @@ public class Loginfo extends HttpServlet {
 		String password = (String) req.getParameter("password");
 		Login a = new Login();
 		if (a.loginst(id, password)) {
+			System.out.println("로그인제출 진입");
 			HttpSession session = req.getSession();
 			session.setAttribute("no", "no");
 			req.getRequestDispatcher("/login.jsp").forward(req, resp);
