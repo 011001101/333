173718c28a9be286eeb2a531479a8d9de2153667
GGG <GGG@DESKTOP-8N3GG4O>
2023-08-01 18:09:40 +0900

    매핑

--- src/plant/Loginfo.java
+++ src/plant/Loginfo.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/login")
+
 public class Loginfo extends HttpServlet {
 
 	@Override
