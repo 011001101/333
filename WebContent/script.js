6d68d6cc62e8bfc365d0ac47085b07303ad8a205
GGG <GGG@DESKTOP-8N3GG4O>
2023-08-08 16:45:07 +0900

    머지수정

--- WebContent/script.js
+++ WebContent/script.js
@@ -1,12 +1,69 @@
+$(document).ready(function(){
+ 
+  $('.BUY>a').on('click', function(){
+      $('.menu_bg').show(); 
+      $('.rabbitFace').show().animate({
+
+        left: '-' + 0 + '%'
+
+      });
+      $('.sidebar_menu').show().animate({
+          right:0
+      });  
+  });
+  $('.close_btn>a').on('click', function(){
+      $('.menu_bg').hide(); 
+      $('.rabbitFace').show().animate({
+
+        left: '+' + 38 + '%'
+
+      });
+      $('.sidebar_menu').animate({
+          right: '-' + 50 + '%'
+                 },function(){
+$('.sidebar_menu').hide(); 
+}); 
+  });
+});
+
+  $(document).ready(function(){
+  $('.SELL>a').on('click', function(){
+      $('.menu_bg2').show(); 
+      $('.rabbitFace').show().animate({
+
+        left: '-' + 0 + '%'
+
+      });
+      $('.sidebar_menu2').show().animate({
+          right:0
+      });  
+  });
+  $('.close_btn2>a').on('click', function(){
+      $('.menu_bg2').hide(); 
+      $('.rabbitFace').show().animate({
+
+        left: '+' + 38 + '%'
+
+      });
+      $('.sidebar_menu2').animate({
+          right: '-' + 50 + '%'
+                 },function(){
+$('.sidebar_menu2').hide(); 
+}); 
+  });
+
+});
+
+
 
 // 인벤 그리기 
 
-const inventorySize = 9;
+const inventorySize = 5;
 let inventory = Array.from({ length: inventorySize }, () => Array(inventorySize).fill(null));
 
 // 인벤토리 그리기
 function drawInventory() {
-    const inventoryContainer = document.getElementById("inventory");
+    const inventoryContainer = document.getElementById("BuyInventory");
     inventoryContainer.innerHTML = "";
 
     for (let row = 0; row < inventorySize; row++) {
@@ -17,8 +74,7 @@ function drawInventory() {
             
             cell.setAttribute("data-row", row);
             cell.setAttribute("data-col", col);
-            cell.draggable=true;
-
+        
             if (inventory[row][col]) {
                 cell.textContent = inventory[row][col];
             }
@@ -29,108 +85,65 @@ function drawInventory() {
     }
 }
 
-// 아이템 추가 또는 제거 
 
-//클릭시 각각의 셀로 상호작용 (안에 메소드 고쳐주시면 됩니다롱) 
-function toggleItem(event) {
-    const row = parseInt(event.target.getAttribute("data-row"));
-    const col = parseInt(event.target.getAttribute("data-col"));
 
-	const buttonId = sessionStorage.getItem("buttonId");
-	sessionStorage.setItem("buttonId", buttonId);
-    if (inventory[row][col]) {
-        inventory[row][col] = null;
-    } else {
-        // 여기에서 아이템 정보를 입력하면 됩니다.
-        // 여기서는 단순히 "아이템"이라는 문자열을 추가합니다.
-		console.log("작동1");
-		if(buttonId != null){
-		   	const r = (row*9)+col;
-		   	sessionStorage.setItem("invencl", r);
-			console.log(r);
-			const xhr = new XMLHttpRequest();
-		   	xhr.open("POST", "/333/game", true);
-		  	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	  		xhr.onreadystatechange = function() {
-		     if (xhr.readyState === 4 && xhr.status === 200) {
-		       	console.log(xhr.responseText);
-		      	window.location.href = "/333/game";
-		      }
-		   };
-		   xhr.send("buttonId=" + encodeURIComponent(buttonId)+"&invencl=" + r);
-		}
-    }
+// 인벤토리 그리기2
+function drawInventory2() {
+  const inventoryContainer = document.getElementById("SellInventory");
+  inventoryContainer.innerHTML = "";
 
-    drawInventory();
+  for (let row = 0; row < inventorySize; row++) {
+      for (let col = 0; col < inventorySize; col++) {
+          const cell = document.createElement("div");
+        
+          cell.classList.add("cell");
+          
+          cell.setAttribute("data-row", row);
+          cell.setAttribute("data-col", col);
+      
+          if (inventory[row][col]) {
+              cell.textContent = inventory[row][col];
+          }
+
+          cell.addEventListener("click", toggleItem2);
+          inventoryContainer.appendChild(cell);
+      }
+  }
 }
+
+
+function toggleItem(event) {
+  const row = parseInt(event.target.getAttribute("data-row"));
+  const col = parseInt(event.target.getAttribute("data-col"));
+
+  if (inventory[row][col]) {
+      inventory[row][col] = null;
+  } else {
+      // 여기에서 아이템 정보를 입력하면 됩니다.
+      // 여기서는 단순히 "아이템"이라는 문자열을 추가합니다.
+      inventory[row][col] = "아이템"+[row]+[col];
+  }
+
+  drawInventory();
+}
+
+
+function toggleItem2(event) {
+  const row = parseInt(event.target.getAttribute("data-row"));
+  const col = parseInt(event.target.getAttribute("data-col"));
+
+  if (inventory[row][col]) {
+      inventory[row][col] = null;
+  } else {
+      // 여기에서 아이템 정보를 입력하면 됩니다.
+      // 여기서는 단순히 "아이템"이라는 문자열을 추가합니다.
+      inventory[row][col] = "아이템"+[row]+[col];
+  }
+
+  drawInventory2();
+}
+
 
 // 인벤토리 초기화
 drawInventory();
-
-
-//인벤 다 그렸고 다 불러왔다
-
-//아이템과 상호작용 하는 함수
-
-
-  // 처음 드래그 요소가 위치하고 있는 인벤 영역
-  const Itemcell = document.querySelector(".cell");
-  
-  Itemcell.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    //console.log(e);
-    //console("드래그 요소가 '첫' 번째 박스 영역에 계속 위치하면 발생하는 이벤트");
-  });
-  Itemcell.addEventListener("drop", (e) => {
-    e.preventDefault();
-    //console.log(e);
-    console.log("드래그 요소가 '첫' 번째 박스 영역에 드롭");
-  });
-  
-  
-  // 드래그 요소가 이동하여 위치할 우측 박스 영역
-  const plantpot = document.querySelector(".plantpot");
-  
-  plantpot.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    //console.log(e);
-    //console("드래그 요소가 '두' 번째 박스 영역에 계속 위치하면 발생하는 이벤트");
-  });
-  plantpot.addEventListener("drop", (e) => {
-    e.preventDefault();
-    //console.log(e);
-    console.log("드래그 요소가 '두' 번째 박스 영역에 드롭");
-  });
-    
-    
-  
-    
-  
-
-
-
-//여기는 메뉴판 스무스하게 움직이게 하는거라 손 안대셔도 됩니당
-$(function() {
-
-    var w = $(window).width(),
-      h = $(window).height();
-    //$('section').width(w);
-    $('section').height(h);
-    $('menu .container').height(h - 60);
-    $('body').prepend('<div id="overlay">');
-  
-    $('#menu').click(function() {$('html').addClass('active');});
-    $('#close-menu').click(function() {$('html').removeClass('active');});
-    $('#overlay').click(function() {$('html').removeClass('active');});
-    $(window).resize(function() {
-      var w = $(window).width(),
-        h = $(window).height();
-      //$('section').width(w);
-      $('section').height(h);
-      $('menu .container').height(h - 60);
-    });
-  
-  });
-
-
-  
+drawInventory2();
